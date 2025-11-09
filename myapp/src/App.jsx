cafca020c0550b7e2678f2e978b22e2a7e102fe7
joonsol <71648638+joonsol@users.noreply.github.com>
2025-11-09 17:08:53 +0900

    Update App.jsx

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>learn githubAction new test testtest 1111</h1>
+      <h1>learn githubAction cicd1 aws1</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
